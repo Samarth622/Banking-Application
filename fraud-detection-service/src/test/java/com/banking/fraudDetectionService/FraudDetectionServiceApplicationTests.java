0f9d4e4379384cba1c2463c0510de58fe7e22fc8
Samarth622 <guptasamarth709@gmail.com>
2026-08-02 11:43:54 +0530

    Full project setup with all the services

---
+++ fraud-detection-service/src/test/java/com/banking/fraudDetectionService/FraudDetectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.fraudDetectionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
