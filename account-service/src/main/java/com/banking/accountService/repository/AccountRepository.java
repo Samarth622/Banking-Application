fdba0d0db3fb7f169ce6e8cd0ba163a00ef2f57a
Samarth622 <guptasamarth709@gmail.com>
2026-08-09 12:05:34 +0530

    Account creation service method with unique account number

--- account-service/src/main/java/com/banking/accountService/repository/AccountRepository.java
+++ account-service/src/main/java/com/banking/accountService/repository/AccountRepository.java
@@ -4,4 +4,8 @@ import com.banking.accountService.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, String> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByAccountNumber(String accountNumber);
 }
--- account-service/src/main/java/com/banking/accountService/service/AccountService.java
+++ account-service/src/main/java/com/banking/accountService/service/AccountService.java
@@ -1,4 +1,79 @@
 package com.banking.accountService.service;
 
+import com.banking.accountService.dto.AccountResponse;
+import com.banking.accountService.dto.CreateAccountRequest;
+import com.banking.accountService.entity.Account;
+import com.banking.accountService.entity.AccountStatus;
+import com.banking.accountService.entity.AccountType;
+import com.banking.accountService.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.security.SecureRandom;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    public AccountResponse createAccount(CreateAccountRequest request) {
+        log.info("Creating account for email: {}", request.getEmail());
+
+        if(accountRepository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("Account already exists");
+        }
+
+        Account account = Account.builder()
+                .email(request.getEmail())
+                .phone(request.getPhone())
+                .accountHolderName(request.getAccountHolderName())
+                .accountType(request.getAccountType())
+                .balance(request.getInitialDeposit())
+                .status(AccountStatus.ACTIVE)
+                .accountNumber(generateAccountNumber())
+                .dailyTransactionLimit(
+                        request.getAccountType() == AccountType.SAVINGS
+                        ? new BigDecimal("100000")
+                        : new BigDecimal("500000")
+                )
+                .build();
+
+        Account savedAccount = accountRepository.save(account);
+        log.info("Account created: {}", savedAccount.getAccountNumber());
+        return mapToResponse(savedAccount);
+    }
+
+    private String generateAccountNumber(){
+        String accountNumber;
+
+        do {
+
+            Long number = secureRandom.nextLong(1_000_000_000_000L);
+            accountNumber = String.format("%012d", number);
+
+        } while(accountRepository.existsByAccountNumber(accountNumber));
+
+        return accountNumber;
+    }
+
+    private AccountResponse mapToResponse(Account account){
+        AccountResponse accountResponse = new AccountResponse();
+        accountResponse.setAccountNumber(account.getAccountNumber());
+        accountResponse.setBalance(account.getBalance());
+        accountResponse.setAccountHolderName(account.getAccountHolderName());
+        accountResponse.setAccountType(account.getAccountType());
+        accountResponse.setEmail(account.getEmail());
+        accountResponse.setPhone(account.getPhone());
+        accountResponse.setDailyTransactionLimit(account.getDailyTransactionLimit());
+        accountResponse.setId(account.getId());
+        accountResponse.setCreatedAt(account.getCreatedAt());
+        accountResponse.setStatus(account.getStatus());
+        return accountResponse;
+    }
 }
