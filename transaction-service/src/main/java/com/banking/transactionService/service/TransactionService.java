e5504a764f11ea99cc68b788b2997064aba91496
Samarth622 <guptasamarth709@gmail.com>
2026-08-19 14:22:39 +0530

    create feign client to call deduct method from account-service, create initiated event for transafer and also create transfer method service

--- transaction-service/src/main/java/com/banking/transactionService/service/TransactionService.java
+++ transaction-service/src/main/java/com/banking/transactionService/service/TransactionService.java
@@ -1,4 +1,92 @@
 package com.banking.transactionService.service;
 
+import com.banking.transactionService.client.AccountServiceClient;
+import com.banking.transactionService.dto.TransactionResponse;
+import com.banking.transactionService.dto.TransferRequest;
+import com.banking.transactionService.entity.Transaction;
+import com.banking.transactionService.entity.TransactionStatus;
+import com.banking.transactionService.entity.TransactionType;
+import com.banking.transactionService.event.TransactionInitiatedEvent;
+import com.banking.transactionService.repository.TransactionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+    private final AccountServiceClient accountServiceClient;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    private static final String TRANSACTION_INITIATED_TOPIC = "transaction.initiated";
+    private static final String TRANSACTION_COMPLETED_TOPIC = "transaction.completed";
+    private static final String TRANSACTION_REFUNDED_TOPIC = "transaction.refunded";
+
+    /**
+     * SAGA Step 1 - Initiate transfer
+     * Deducts from sender by feign
+     * Saves transaction as PROCESSING
+     * Publish event for Kafka for fraud check
+     * Returns
+     * @param request
+     * @return
+     */
+    public TransactionResponse transfer(TransferRequest request) {
+        log.info("SAGA Start - Transfer: {} -> {}, amount: {}",
+                request.getSenderAccountNumber(),
+                request.getReceiverAccountNumber(),
+                request.getAmount());
+
+        accountServiceClient.deductBalance(
+                request.getSenderAccountNumber(),
+                request.getAmount()
+        );
+
+        Transaction transaction = new Transaction();
+        transaction.setSenderAccountNumber(request.getSenderAccountNumber());
+        transaction.setReceiverAccountNumber(request.getReceiverAccountNumber());
+        transaction.setAmount(request.getAmount());
+        transaction.setType(TransactionType.TRANSFER);
+        transaction.setStatus(TransactionStatus.PROCESSING);
+        transaction.setDescription(request.getDescription());
+        transaction.setReferenceNumber(UUID.randomUUID().toString());
+
+        Transaction savedTransaction = transactionRepository.save(transaction);
+        log.info("Transaction saved as PROCESSING: {}", savedTransaction.getId());
+
+        TransactionInitiatedEvent event = new TransactionInitiatedEvent(
+                savedTransaction.getId(),
+                savedTransaction.getSenderAccountNumber(),
+                savedTransaction.getReceiverAccountNumber(),
+                savedTransaction.getAmount(),
+                savedTransaction.getDescription()
+        );
+
+        kafkaTemplate.send(TRANSACTION_INITIATED_TOPIC, savedTransaction.getId(), event);
+        log.info("TransactionInitiatedEvent saved as PROCESSING: {}", savedTransaction.getId());
+
+        return mapToResponse(savedTransaction);
+    }
+
+    private TransactionResponse mapToResponse(Transaction transaction) {
+        TransactionResponse transactionResponse = new TransactionResponse();
+        transactionResponse.setId(transaction.getId());
+        transactionResponse.setSenderAccountNumber(transaction.getSenderAccountNumber());
+        transactionResponse.setReceiverAccountNumber(transaction.getReceiverAccountNumber());
+        transactionResponse.setAmount(transaction.getAmount());
+        transactionResponse.setStatus(transaction.getStatus());
+        transactionResponse.setType(transaction.getType());
+        transactionResponse.setDescription(transaction.getDescription());
+        transactionResponse.setReferenceNumber(transaction.getReferenceNumber());
+        transactionResponse.setCreatedAt(transaction.getCreatedAt());
+        transactionResponse.setCompletedAt(transaction.getCompletedAt());
+        transactionResponse.setFailureReason(transaction.getFailureReason());
+        return transactionResponse;
+    }
 }
